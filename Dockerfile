2c9dc09216916a317191c2027809cbc64655dc8b
Francesc Lapedriza <francesc.lapedriza@immfly.com>
2018-07-12 19:46:23 +0200

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,15 @@ FROM node:10.6.0
 ENV NPM_CONFIG_LOGLEVEL warn
 
 RUN mkdir -p /app
+
+RUN mkdir -p /app/src
+
 WORKDIR /app
 
 COPY package.json /app
 COPY package-lock.json /app
+COPY webpack.config.js /app
+COPY .babelrc /app
 
 RUN npm install
 
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pwd=$(pwd)
 
 printf "\n\nStopping docker image with name '$1'\n"
 docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{ print $1 }' | xargs -I {} docker stop {}
@@ -8,4 +7,4 @@ printf "\n\nBuilding docker image with name '$1'\n"
 docker build ./ -t $1
 printf "\nDocker image with name '$1' built\n"
 printf "\n\nRuning docker image with name '$1' on port $2\n"
-docker run -t -v $pwd:/app -p $2:80 -e PORT=$2 $1 
+docker run -t -v $PWD/src:/app/src -p $2:80 -e PORT=$2 $1 
